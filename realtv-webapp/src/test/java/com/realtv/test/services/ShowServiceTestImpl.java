bae0444473b43b1d05e604f40351da146c36ff7f
Arthur Portas <arthurportas@gmail.com>
2014-01-01 19:44:40 +0000

    Updated junit and added harmcrest

--- realtv-webapp/src/test/java/com/realtv/test/services/ShowServiceTestImpl.java
+++ realtv-webapp/src/test/java/com/realtv/test/services/ShowServiceTestImpl.java
@@ -6,11 +6,12 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
-import junit.framework.Assert;
-
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -18,8 +19,16 @@ import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import com.realtv.domain.Show;
 import com.realtv.services.ShowService;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertEquals;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:test-context.xml",
@@ -39,18 +48,29 @@ public class ShowServiceTestImpl implements ShowServiceTest {
 
 	private Show mockedShow;
 
+	@Mock
+	private Show _mockedShow;
+
 	@Before
 	public void setup() {
 		mockedShow = new Show();
 		mockedShow.setBeginning(Calendar.getInstance().getTime());
+		MockitoAnnotations.initMocks(this);
+	}
+
+	@After
+	public void tearDown() {
+		showService = null;
+		mockedShow = null;
+		_mockedShow = null;
 	}
 
 	@Test
 	@Override
 	public void create() {
 		slf4jLogger.info("==create()==");
-		Assert.assertNotNull("show should not be null",
-				showService.create(mockedShow));
+		assertNotNull("show should not be null", showService.create(mockedShow));
+
 	}
 
 	@Test
@@ -58,15 +78,14 @@ public class ShowServiceTestImpl implements ShowServiceTest {
 	public void delete() {
 		slf4jLogger.info("==delete()==");
 		final Show show = showService.create(mockedShow);
-		Assert.assertNotNull("show should not be null", show);
-		Assert.assertNotNull("show should not be null", show.getId());
+		assertNotNull("show should not be null", show);
+		assertNotNull("show should not be null", show.getId());
 		// It should be found
-		Assert.assertNotNull("show should not be null",
-				showService.find(show.getId()));
+		assertNotNull("show should not be null", showService.find(show.getId()));
 
 		showService.delete(show.getId());
 		// It should not be found
-		Assert.assertNull("show should be null", showService.find(show.getId()));
+		assertNull("show should be null", showService.find(show.getId()));
 	}
 
 	@Test
@@ -74,17 +93,17 @@ public class ShowServiceTestImpl implements ShowServiceTest {
 	public void update() {
 		slf4jLogger.info("==update()==");
 		Show show = showService.create(mockedShow);
-		Assert.assertNotNull("show should not be null", show);
-		Assert.assertNotNull("show should not be null", show.getId());
+		assertNotNull("show should not be null", show);
+		assertNotNull("show should not be null", show.getId());
 		show.setName("show updated");
 
 		showService.update(show);
 		final Show showUpdated = showService.find(show.getId());
-		Assert.assertNotNull("showUpdated should not be null", showUpdated);
-		Assert.assertNotNull("showUpdated should not be null",
-				showUpdated.getId());
-		Assert.assertEquals(show.getId(), showUpdated.getId());
-		Assert.assertTrue(showUpdated.getName().equals("show updated"));
+		assertNotNull("showUpdated should not be null", showUpdated);
+		assertNotNull("showUpdated should not be null", showUpdated.getId());
+		assertEquals(show.getId(), showUpdated.getId());
+		assertThat(show.getId(), equalTo( showUpdated.getId()));
+		assertTrue(showUpdated.getName().equals("show updated"));
 	}
 
 	@Test
@@ -92,33 +111,32 @@ public class ShowServiceTestImpl implements ShowServiceTest {
 	public void find() {
 		slf4jLogger.info("==find()==");
 		final Show show = showService.create(mockedShow);
-		Assert.assertNotNull("show should not be null", show);
-		Assert.assertNotNull("show should not be null", show.getId());
+		assertNotNull("show should not be null", show);
+		assertNotNull("show should not be null", show.getId());
 		// It should be found
-		Assert.assertNotNull("show should not be null",
-				showService.find(show.getId()));
+		assertNotNull("show should not be null", showService.find(show.getId()));
 	}
 
 	@Test
 	@Override
 	public void getAll() {
 		slf4jLogger.info("==getAll()==");
-		Assert.assertTrue(showService.getAll().size() > 0);
+		assertTrue(showService.getAll().size() > 0);
 	}
 
 	@Test
 	@Override
 	public void findAllNamedQuery() {
 		slf4jLogger.info("==findAllNamedQuery()==");
-		Assert.assertTrue(showService.findAllNamedQuery().size() > 0);
+		assertTrue(showService.findAllNamedQuery().size() > 0);
 	}
 
 	@Test
 	@Override
 	public void count() {
 		slf4jLogger.info("==count()==");
-		Assert.assertTrue(showService.getAll().size() >= 11);
-		Assert.assertNotNull("show should not be null", mockedShow.toJson());
+		assertTrue(showService.getAll().size() >= 11);
+		assertNotNull("show should not be null", mockedShow.toJson());
 		slf4jLogger.debug(" mockedShow.toJson()->" + mockedShow.toJson());
 	}
 
@@ -133,6 +151,6 @@ public class ShowServiceTestImpl implements ShowServiceTest {
 		query.setParameter(2, "Quem Quer Ser Milionário.");
 		query.setParameter(3, "2012-12-26 05:22:30");
 		int res = query.executeUpdate();
-		Assert.assertEquals(res, 1);
+		assertEquals(res, 1);
 	}
 }
